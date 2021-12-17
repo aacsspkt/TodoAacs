3f35fdf47510f16dc2f242b6586874d85be29137
Ashish Sapkota <ashishspkt6566@gmail.com>
2021-12-17 23:41:22 +0545

    RecyclerView added in Main Activity, themes and color set

--- app/src/main/java/com/aacs/todoaacs/MainActivity.java
+++ app/src/main/java/com/aacs/todoaacs/MainActivity.java
@@ -1,14 +1,21 @@
 package com.aacs.todoaacs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewTodos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.rvTodoList);
     }
 }
