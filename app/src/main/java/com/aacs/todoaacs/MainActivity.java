09ba50786d709e0e84e67ea8a9bde19acc741919
Ashish Sapkota <ashishspkt6566@gmail.com>
2021-12-18 10:21:52 +0545

    Update MainActivity.java
    Implemented FragmentChangeListener

--- app/src/main/java/com/aacs/todoaacs/MainActivity.java
+++ app/src/main/java/com/aacs/todoaacs/MainActivity.java
@@ -1,13 +1,18 @@
 package com.aacs.todoaacs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.aacs.todoaacs.listener.FragmentChangeListener;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentChangeListener {
     private RecyclerView recyclerViewTodos;
 
 
@@ -17,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.rvTodoList);
+    }
+
+    @Override
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();;
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment, fragment.toString());
+        fragmentTransaction.addToBackStack(fragment.toString());
+        fragmentTransaction.commit();
     }
 }
