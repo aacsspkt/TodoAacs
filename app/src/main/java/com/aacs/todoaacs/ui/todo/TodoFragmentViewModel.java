7444b20b9d63f91418c5da329e3b5aaeffd63250
Ashish Sapkota <ashishspkt6566@gmail.com>
2021-12-22 00:22:08 +0545

    Update TodoFragmentViewModel.java
    - Re-modified view model to map the view better

--- app/src/main/java/com/aacs/todoaacs/ui/todo/TodoFragmentViewModel.java
+++ app/src/main/java/com/aacs/todoaacs/ui/todo/TodoFragmentViewModel.java
@@ -10,33 +10,52 @@ import androidx.lifecycle.MutableLiveData;
 import com.aacs.todoaacs.model.TodoModel;
 import com.aacs.todoaacs.model.TodoStatus;
 import com.aacs.todoaacs.repository.TodoRepository;
-import com.aacs.todoaacs.util.DateUtility;
 
 import java.util.Date;
 
 public class TodoFragmentViewModel extends AndroidViewModel {
     private final TodoRepository repository;
-    private MutableLiveData<TodoModel> todo;
-
+    private int uid;
+    private MutableLiveData<String> task;
+    private MutableLiveData<Date> date;
+    private TodoStatus todoStatus;
 
     public TodoFragmentViewModel(@NonNull Application application) {
         super(application);
         repository = new TodoRepository(application);
-        todo = new MutableLiveData<>();
     }
 
-    public MutableLiveData<TodoModel> getTodoLiveData() {
-        if (todo == null) {
-            todo = new MutableLiveData<>();
-        }
-        return todo;
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public TodoStatus getTodoStatus() {
+        return todoStatus;
+    }
+
+    public void setTodoStatus(TodoStatus status) {
+        this.todoStatus = status;
+    }
+
+    public MutableLiveData<String> getTask() {
+        if (task == null) task = new MutableLiveData<>();
+        return task;
+    }
+
+    public MutableLiveData<Date> getDate() {
+        if (date == null) date = new MutableLiveData<>();
+        return date;
     }
 
     public void addTodo(TodoModel todo) {
         repository.addTodo(todo);
     }
 
-    public void updateTodo(TodoModel todo){
+    public void updateTodo(TodoModel todo) {
         repository.updateTodo(todo);
     }
 
