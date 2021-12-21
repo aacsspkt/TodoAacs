14bb22ad2341790b6789cfde6d22ea7efa567791
Ashish Sapkota <ashishspkt6566@gmail.com>
2021-12-21 09:00:30 +0545

    Update TodoListFragment.java
    - set Fragment result listeners
    - add checkbox listeners
    - renamed showTodoFragment as createNewTodo
    - code optimized

--- app/src/main/java/com/aacs/todoaacs/ui/todolist/TodoListFragment.java
+++ app/src/main/java/com/aacs/todoaacs/ui/todolist/TodoListFragment.java
@@ -1,109 +1,140 @@
 package com.aacs.todoaacs.ui.todolist;
 
+import android.annotation.SuppressLint;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.aacs.todoaacs.adapter.TodoListAdapter;
 import com.aacs.todoaacs.databinding.TodoListFragmentBinding;
-import com.aacs.todoaacs.listener.DeleteButtonClickListener;
+import com.aacs.todoaacs.listener.CheckBoxCheckedChangeListener;
 import com.aacs.todoaacs.listener.FragmentChangeListener;
 import com.aacs.todoaacs.listener.RecyclerViewItemClickListener;
 import com.aacs.todoaacs.model.TodoModel;
-import com.aacs.todoaacs.ui.SharedViewModel;
+import com.aacs.todoaacs.model.TodoStatus;
 import com.aacs.todoaacs.ui.todo.TodoFragment;
 
 import java.util.ArrayList;
 
 public class TodoListFragment extends Fragment {
-    private final String TAG = getTag();
+    public static final String RESUlT = "Result";
+    public static final String REQUEST_KEY = "Request_Key";
+    private final String TAG = this.toString();
     private RecyclerViewItemClickListener recyclerViewItemClickListener;
-    private DeleteButtonClickListener deleteButtonClickListener;
-    private TodoListViewModel todoListViewModel;
-    private SharedViewModel sharedViewModel;
+    private RecyclerViewItemClickListener deleteButtonClickListener;
+    private CheckBoxCheckedChangeListener checkBoxClickListener;
+    private TodoListFragmentViewModel todoListFragmentViewModel;
+    private TodoListFragmentBinding binding;
     private TodoListAdapter adapter;
 
-    public static TodoListFragment newInstance() {
-        return new TodoListFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         Log.w(TAG, "Fragment View Created");
 
-        TodoListFragmentBinding binding = TodoListFragmentBinding.inflate(getLayoutInflater());
-        todoListViewModel = new ViewModelProvider(this).get(TodoListViewModel.class);
 
+        binding = TodoListFragmentBinding.inflate(getLayoutInflater());
+        todoListFragmentViewModel = new ViewModelProvider(this).get(TodoListFragmentViewModel.class);
 
-        setViewModelObserver(getViewLifecycleOwner(), binding);
-        binding.buttonCreateNewTodo.setOnClickListener(this::showTodoFragment);
+        checkFragmentResults();
+        setRecyclerViewItemClickListener();
+        setDeleteButtonClickListener();
+        setCheckBoxClickListener();
+        initRecyclerView();
+        setViewModelObserver(getViewLifecycleOwner());
+
+        binding.buttonCreateNewTodo.setOnClickListener(this::createNewTodo);
 
         return binding.getRoot();
     }
 
-    private void setAdapter(ArrayList<TodoModel> todos) {
-        adapter = new TodoListAdapter(
-                recyclerViewItemClickListener,
-                deleteButtonClickListener
-        );
-        adapter.setTodos(todos);
+    private void checkFragmentResults() {
+        getParentFragmentManager().setFragmentResultListener(
+                REQUEST_KEY,
+                this,
+                (requestKey, result) -> Toast.makeText(getContext(),
+                        result.getString(RESUlT),
+                        Toast.LENGTH_SHORT).show()
+                );
     }
 
-    private void setViewModelObserver(LifecycleOwner owner, TodoListFragmentBinding binding) {
-        final Observer<ArrayList<TodoModel>> todoListObserver = todoModels -> {
-            if (todoModels.size() > 0) {
-                setRecyclerViewItemClickListener(todoModels);
-                setDeleteButtonClickListener();
-                setAdapter(todoModels);
-                initRecyclerView(binding);
-            }
-        };
+    @SuppressLint("NotifyDataSetChanged")
+    private void setViewModelObserver(LifecycleOwner owner) {
+        todoListFragmentViewModel.getTodosLiveData().observe(owner, todoModels -> {
+                    Log.w(TAG, "Todos Arraylist Changed.");
+                    adapter.setTodos((ArrayList<TodoModel>) todoModels);
+                }
+        );
+    }
 
-        todoListViewModel.getTodosLiveData().observe(getViewLifecycleOwner(), todoListObserver);
+    private void setCheckBoxClickListener() {
+        checkBoxClickListener = (view, b, todo, textView) -> {
+            if (b) {
+                todo.setIsCompleted(TodoStatus.YES);
+                textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            } else {
+                todo.setIsCompleted(TodoStatus.NO);
+                textView.setPaintFlags(0);
+            }
+            todoListFragmentViewModel.updateTodo(todo);
+            Log.w(TAG, "Todo with uid " + todo.getUid() + " Updated");
+        };
     }
 
     private void setDeleteButtonClickListener() {
-        deleteButtonClickListener = (view, position) -> {
-            Log.d(TAG, "Delete Button Clicked.");
-            // todo do something to remove todo at this position.
+        deleteButtonClickListener = (view, todo) -> {
+            todoListFragmentViewModel.deleteTodo(todo);
+            Log.w(TAG, "Todo with uid " + todo.getUid() + " Deleted");
         };
     }
 
-    private void setRecyclerViewItemClickListener(ArrayList<TodoModel> todos) {
-        recyclerViewItemClickListener = (view, position) -> {
-            Log.d(TAG, "Recycle View Item Clicked.");
-            sharedViewModel.select(todos.get(position));
-        };
+    private void setRecyclerViewItemClickListener() {
+        recyclerViewItemClickListener = this::updateTodo;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+    private void updateTodo(View view, TodoModel todo) {
+        Log.d(TAG, "Recycle View Item Clicked.");
+        Bundle bundle = new Bundle();
+        bundle.putInt(TodoFragment.UID, todo.getUid());
+        Log.d(TAG,
+                "Argument Sent: UID = " + todo.getUid());
+        TodoFragment todoFragment = new TodoFragment();
+        todoFragment.setArguments(bundle);
+        FragmentChangeListener listener = (FragmentChangeListener) getActivity();
+        if (listener != null) {
+            listener.replaceFragment(todoFragment);
+        } else {
+            Log.d(TAG, "Fragment not associated with Activity");
+        }
     }
 
-    private void initRecyclerView(TodoListFragmentBinding binding) {
+    private void initRecyclerView() {
+        adapter = new TodoListAdapter(
+                recyclerViewItemClickListener,
+                deleteButtonClickListener,
+                checkBoxClickListener
+        );
         binding.recyclerViewTodoList.setAdapter(adapter);
         binding.recyclerViewTodoList.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
-    private void showTodoFragment(View view) {
+    private void createNewTodo(View view) {
         Log.d(TAG, "Floating Action Button Clicked");
-        TodoFragment todoFragment = new TodoFragment();
         FragmentChangeListener listener = (FragmentChangeListener) getActivity();
         if (listener != null) {
-            listener.replaceFragment(todoFragment);
+            listener.replaceFragment(new TodoFragment());
         } else {
             Log.d(TAG, "Fragment not associated with Activity");
         }
