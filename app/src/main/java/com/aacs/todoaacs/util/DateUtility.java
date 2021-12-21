ec54e12d4a790e294326d05497f7adbdc54bfe07
Ashish Sapkota <ashishspkt6566@gmail.com>
2021-12-21 08:47:47 +0545

    Update DateUtility.java
    create method that return today's date

--- app/src/main/java/com/aacs/todoaacs/util/DateUtility.java
+++ app/src/main/java/com/aacs/todoaacs/util/DateUtility.java
@@ -2,6 +2,7 @@ package com.aacs.todoaacs.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -14,8 +15,20 @@ public class DateUtility {
         return format.parse(date);
     }
 
-    public static String dateToString(String patten, Date date){
-        format = new SimpleDateFormat(patten, Locale.ENGLISH);
+    public static String dateToString(String pattern, Date date) {
+        format = new SimpleDateFormat(pattern, Locale.ENGLISH);
         return format.format(date);
+    }
+
+    public static Date today(String pattern) {
+        format = new SimpleDateFormat(pattern, Locale.ENGLISH);
+        String formattedDate = format.format(Calendar.getInstance().getTime());
+        Date today = null;
+        try {
+            today = format.parse(formattedDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return today;
     }
 }
